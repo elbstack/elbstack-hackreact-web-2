1124410391d797389efe99fc370b4bea14b2a76a
Jose St Bernard <j.bernard@liquidlabs.de>
2016-03-05 14:22:52 +0100

    Add channels list

--- src/components/containers/ChannelList.js
+++ src/components/containers/ChannelList.js
@@ -4,24 +4,31 @@ import styles from './ChannelList.scss'
 import { listChannels } from '../../redux/actions/channels'
 
 @connect(
+  state => ({
+    channelList: state.channels.list
+  }),
   dispatch => ({
     listChannels: () => dispatch(listChannels())
   })
 )
 export default class ChannelList extends Component {
 
+  var state = {
+    visible: false
+  }
   componentWillMount() {
     this.props.listChannels()
   }
 
   render() {
 
-    const content = <p>Lade Channels</p>
-
+    const channels = this.props.channelList || {}
     return (
       <div className={styles.container}>
         <div className={styles.list}>
-          {content}
+          <ul>
+          { Object.keys(channels).map((itemKey)=> <li> {channels[itemKey].name}</li>) }
+          </ul>
         </div>
       </div>
     )
--- src/components/views/Messenger.js
+++ src/components/views/Messenger.js
@@ -6,13 +6,22 @@ import JoinedChannelList from '../elements/JoinedChannelList'
 import TeamLogo from '../elements/TeamLogo'
 import IntroText from '../elements/IntroText'
 import styles from './Messenger.scss'
+import ChannelList from '../containers/ChannelList'
 
 @connect(
   state => ({
     sendbird: state.sendbird
   })
 )
+
+function toggleChannelList(visible){
+  return !visible
+}
+
 export default class Messenger extends Component {
+  var state = {
+    channelListVisible : false
+  }
 
   render() {
     return (
@@ -20,10 +29,11 @@ export default class Messenger extends Component {
         <div className={styles.nav}>
           <TeamLogo/>
           <Profile userName={this.props.sendbird.user_name}/>
-          <ListChannelsBtn />
+          <ListChannelsBtn onClick={this.toggleChannelList}/>
           <JoinedChannelList />
         </div>
         <div className={styles.chat}>
+          <ChannelList />
           <IntroText />
         </div>
       </div>
